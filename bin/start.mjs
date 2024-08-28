735d115cf970bcac9c1cd15e572e6106cce14c02
NajeedUrRehman <74587097+najeedurrehman@users.noreply.github.com>
2024-08-28 21:08:06 +0500

    SETUP MONGODB CONNECTION

--- bin/start.mjs
+++ bin/start.mjs
@@ -1,8 +1,16 @@
 import app from "../app.mjs";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  const { port } = server.address();
-  console.log(`SERVER START ON PORT => ${port}`);
-});
+try {
+  await mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE}`);
+  const server = app.listen(process.env.PORT || 3000, () => {
+    const { port } = server.address();
+    console.log(`SERVER START ON PORT => ${port}`);
+  });
+} catch (err) {
+  const { message } = err;
+  console.error(message);
+  process.exit(1);
+}
